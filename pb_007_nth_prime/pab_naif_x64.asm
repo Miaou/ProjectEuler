451f98430de32c50e8006c2a2e6b7cc8ede73bd2
Miaou <miaou.pbl@gmail.com>
2015-11-04 10:59:47 +0100

    Enieme nombre premier en assembleur x86-64: code intéressants mais résultats décourageants !!!

--- pb_007_nth_prime/pab_naif_x64.asm
+++ pb_007_nth_prime/pab_naif_x64.asm
@@ -8,11 +8,12 @@ extern      printf, sscanf
 
 
 SECTION     .data
-    sFmt:           db  "iPrime: %d", 0x0A, 0 ; 0x0A is '\n'...
+    sFmt:           db  "pab_naif_asm64(%lld) = %lld", 0x0A, 0 ; 0x0A is '\n'...
     sFmtScanf:      db  "%ld", 0
     sErrNoPrime:    db  "Il faut donner l'indice du nombre à trouver", 0x0A, 0
 SECTION     .bss
-    iPrime:         resw    4
+    iPrime:         resw    1
+    root:           resq    1
 
 SECTION     .text
 
@@ -20,8 +21,9 @@ SECTION     .text
 main:
 
   get_nth_prime:
+    push    rbx
     dec     rdi
-    jz     no_prime
+    jz      no_prime
 
     ;add     rsi, 8                      ; argv++
     mov     rdi, [rsi+8]
@@ -32,15 +34,65 @@ main:
     call    sscanf
     test    rax, rax
     jz      no_prime
+    mov     r10, [iPrime]
 
-    mov     rsi, [iPrime]
+  ; rcx: i principal
+  ; rdx:rax: pour la division
+  ; rbx: j
+  ; r8: iPrime
+  ; r9: root
+  ;; r10: bPrime
+  ; r10: iPrime Origine
+  f_naive:
+    mov     r8, [iPrime]
+    ; si iPrime < 2, on renvoie 2
+    mov     rcx, 2
+    cmp     r8, 2
+    jb      show_result
+
+    mov     rcx, 2
+   loop_i:
+    cmp     r8, 1
+    jb      show_result
+    mov     [root], rcx
+    fild    qword [root]
+    fsqrt
+    fistp   qword [root]
+    mov     r9, [root]
+
+    mov     rbx, 2
+   loop_j:
+    mov     rax, rcx
+    cqo
+    div     rbx
+    test    rdx, rdx
+    jz      not_prime
+    inc     rbx
+    cmp     rbx, r9
+    jbe     loop_j
+   
+   ;prime:
+    dec     r8
+    cmp     r8, 1
+    jz      show_result
+   not_prime:
+    inc     rcx
+    jmp     loop_i
+
+  show_result:
     mov     rdi, sFmt
+    mov     rsi, r10
+    mov     rdx, rcx
     mov     al, 0                       ; x86_64 ABI: varags should specify the number of vector args in al...
     call    printf
-    ret
+    jmp     end
 
   no_prime:
     mov     rdi, sErrNoPrime
     mov     al, 0
     call    printf
+
+  end:
+    pop     rbx
+    xor     rax, rax
     ret
