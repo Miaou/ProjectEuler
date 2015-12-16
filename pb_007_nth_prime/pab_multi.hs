8b795498762ed14a9013e245e18af9ad53c8123f
Miaou <miaou.pbl@gmail.com>
2015-12-16 15:02:09 +0100

    Sieve Haskell

--- pb_007_nth_prime/pab_multi.hs
+++ pb_007_nth_prime/pab_multi.hs
@@ -4,6 +4,8 @@ import System.Environment   -- getArgs
 import Text.Printf
 import System.IO            -- stderr
 import System.CPUTime
+import qualified Data.Set as Set -- sieve functions
+import Debug.Trace
 
 
 -- See https://wiki.haskell.org/Timing_computations for better time computations
@@ -41,3 +43,23 @@ primes_opt n = do
     let ps = 2:[x | x <- [3..],
                 all (\y -> mod x y > 0) $ takeWhile (<= (floor $ sqrt $ fromIntegral x)) ps]
     ps !! (n-1)
+
+-- Computes a sieve up to number N
+--  (present numbers are primes)
+-- Lente car fait beaucoup de constructions d'arbre
+--sieve n [] = []
+--sieve n (p:ps) = p : (sieve n (Set.toList $ Set.difference (Set.fromList ps) (Set.fromList [2*p,3*p..n])))
+-- Gourmande car stoque les sp intermédiare
+--sieve n [] _ = []
+--sieve n (p:ps) sp = p : (sieve n (filter (\x -> not $ Set.member x sp) ps) (Set.union sp (Set.fromList [2*p,3*p..n])))
+-- Il faudrait pouvoir executer un for sans conséquences autres que la modif de sp
+--sieve n [] _ = []
+--sieve n (p:ps) sp = p : (sieve n (filter (\x -> not $ Set.member x sp) ps) (foldl (\s e -> Set.insert e s) sp [2*p,3*p..n]))
+-- Reste le filter
+sieve_calc n [] _ = []
+sieve_calc n (p:ps) sp = p : (sieve_calc n ps (foldl (\s e -> Set.insert e s) sp [2*p,3*p..n]))
+sieve n = do
+    let sp = Set.empty :: Set.Set Int
+    let ps = filter (\x -> not $ Set.member x sp) [2..n]
+    let res = sieve_calc n ps sp
+    trace ("sp "++show sp) res
