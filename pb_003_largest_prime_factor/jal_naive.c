28003c09c4bf001085b7c28d6930088241bf6f01
Joel Allred <jalbait@caramail.fr>
2016-07-19 18:40:06 +0200

    Alternative version

--- pb_003_largest_prime_factor/jal_naive.c
+++ pb_003_largest_prime_factor/jal_naive.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Euler Problem no 3
  *
  * Find the largest prime divisor of a given number.
@@ -10,26 +10,26 @@
 #include <limits.h>
 #include <time.h>
 
-long long int compute_rec( long long int n, long long int last_n )
+unsigned long long int compute_rec( unsigned long long int n, unsigned long long int last_n )
 {
-  long long int i;
-  long long int sqrt_n;
+  unsigned long long int i;
+  unsigned long long int sqrt_n;
   long double n_d;
+  int prime_div;
 
-  if ( n >= LLONG_MAX )
+  if ( n >= ULLONG_MAX )
   {
     printf( "ERROR: Overflow\n");
     return -1;
   }
 
   n_d = (long double) n;
-  sqrt_n = sqrtl( n_d ); 
 
   if ( n == 2 )
   {
     return n;
   }
-  
+
   if ( n <= 2 )
   {
     return last_n;
@@ -40,29 +40,23 @@ long long int compute_rec( long long int n, long long int last_n )
     return compute_rec( n / 2, n );
   }
 
+  sqrt_n = sqrtl( n_d );
 
   for ( i = 3; i <= sqrt_n; i = i + 2 )
   {
-    long long int j, sqrt_i;
-
-    if ( i % 2 == 0 )
+    if ( n % i == 0 )
     {
-      continue;
-    }
+#if 1
+      unsigned long long int toto = n / i;
 
-    sqrt_i = sqrtl(i);
-
-    for ( j = 3; j <= sqrt_i; j = j + 2 )
-    {
-      if ( i % j == 0 )
+      while ( toto % i == 0 )
       {
-        continue;
+        toto = toto / i ;
       }
-    }
-
-    if (n % i == 0)
-    {
-      return compute_rec( n / i, n );
+      return compute_rec( toto, toto * i );
+#else
+      return compute_rec( n / i , n );
+#endif
     }
   }
 
@@ -71,23 +65,23 @@ long long int compute_rec( long long int n, long long int last_n )
 
 int main( int argc, char **argv )
 {
-  long long int input;
+  unsigned long long int input;
 
   if( argc != 2 )
   {
     printf( "Give the number for which to compute the greatest prime divisor.\n" );
       return 1;
   }
-    
-  if( sscanf(argv[1], "%lld", &input) != 1 )
+
+  if( sscanf(argv[1], "%llu", &input) != 1 )
   {
     printf( "Can't read input \"%s\"\n", argv[1] );
     return 2;
   }
 
   clock_t begin = clock();
-  printf("Input   : %lld\n", input );
-  printf("Result  : %lld\n", compute_rec( input, input ) );
+  printf("Input   : %llu\n", input );
+  printf("Result  : %llu\n", compute_rec( input, input ) );
   clock_t end = clock();
   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   printf("Time    : %f ms\n", time_spent * 1000 );
