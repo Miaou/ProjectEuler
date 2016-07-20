eda1cd7f71d8e3d5121876a4cd3ed05b332361e4
Billaude <Billaude@users.noreply.github.com>
2016-07-20 20:00:22 +0200

    Pb 4 update to new makefile.

--- pb_004_largest_palindrome_product/ebi_naif.hs
+++ pb_004_largest_palindrome_product/ebi_naif.hs
@@ -1,39 +1,15 @@
 module MainEBINaif where
 
-import System.Environment   -- getArgs
-import Text.Printf
-import System.IO            -- stderr
-import System.CPUTime
+import Euler.Utils
 
 main :: IO()
 main = do
-    args <- getArgs
-    if length args /= 1
-      then echo_err "There shall be exactly one argument."
-      else let lim = read $ args !! 0 :: Integer in do
-        (t,v) <- time getHighestPal lim
-        printf "Input  : %d\n" lim
-        printf "Result : %d\n" (v::Integer)
-        printf "Time   : %.3fs\n" (t::Float)
+    lim <- read_int
+    stat_func getHighestPal lim
 
-echo_err str = hPutStrLn stderr str 
 
--- See https://wiki.haskell.org/Timing_computations for better time computations
-time f v = do
-    start <- getCPUTime
-    x <- return $! f v
-    end <- getCPUTime
-    return ((fromIntegral (end-start))*1e-12, x)
+getHighestPal p = let interv = [10^(p-1) .. 10^p - 1] in
+                  maximum . filter isPal . map (\(x, y) -> x*y) $ [(x, y) | x <- interv, y <- interv, x <= y]
 
-getAllPal x = let maxi = (10^x - 1) ;
-                  mini = 10^(x-1) ;
-                  interv = [maxi, maxi-1 .. mini] ;
-                  cartesianProd = [(x, y) | x <- interv, y <- interv, x <= y] ;
-                  products = map (\(x, y) -> x*y) cartesianProd in
-                    filter (\x -> isPalindrome x) products
-                
-
-getHighestPal x = maximum $ getAllPal x
-
-isPalindrome x = let x_str = show x in (==) x_str (reverse x_str)
+isPal x = let x_str = show x in (==) x_str (reverse x_str)
 
