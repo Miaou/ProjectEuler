61ac66b7ded0c9cb0e26274ad9aa32e04860bb13
Billaude <Billaude@users.noreply.github.com>
2016-07-20 20:04:39 +0200

    Pb 1 update to new makefile.

--- pb_001_multiple_3_5/ebi_solution.hs
+++ pb_001_multiple_3_5/ebi_solution.hs
@@ -1,35 +1,18 @@
-module Main where
+module MainEBISolution where
 
-import System.Environment   -- getArgs
-import Text.Printf
-import System.IO            -- stderr
-import System.CPUTime
+import Euler.Utils
 
 main :: IO()
 main = do
-    args <- getArgs
-    if length args /= 1
-      then echo_err "There shall be exactly one argument."
-      else let lim = read $ args !! 0 :: Integer in do
-        (t,v) <- time get_sum_3_5 lim
-        printf "Input  : %d\n" lim
-        printf "Result : %d\n" (v::Integer)
-        printf "Time   : %.3fs\n" (t::Float)
+    lim <- read_int
+    stat_func get_sum_3_5 lim
 
-echo_err str = hPutStrLn stderr str 
-
--- See https://wiki.haskell.org/Timing_computations for better time computations
-time f v = do
-    start <- getCPUTime
-    x <- return $! f v
-    end <- getCPUTime
-    return ((fromIntegral (end-start))*1e-12, x)
 
 get_sum_3_5 :: (Integral a) => a -> a
 get_sum_3_5 target = (get_sum target 3) + (get_sum target 5) - (get_sum target 15)
 
 -- SUM(x, 1, N) = N * (N + 1) / 2
 get_sum :: (Integral a) => a -> a -> a
-get_sum target multiple = let higher_bound = (target - 1) `div` multiple
+get_sum target multiple = let higher_bound = div (target - 1) multiple
                           in multiple * higher_bound * (higher_bound + 1) `div` 2
 
