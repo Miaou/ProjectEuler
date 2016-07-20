425df4a5281e2422e07271f47b6ebc178ca0a9ba
Billaude <Billaude@users.noreply.github.com>
2016-07-20 19:58:12 +0200

    Pb 8 update to new makefile.

--- pb_008_largest_product_in_series/ebi_naif.hs
+++ pb_008_largest_product_in_series/ebi_naif.hs
@@ -1,23 +1,18 @@
 module MainEBINaif where
 
-import Data.List
 import Data.List.Split
 import Euler.Utils
-import Text.Printf (printf)
 
 main :: IO()
 main = do
-    (input_file_name, nb_digits) <- read_string_int
-    (t,v) <- time_3args getLargestProduct input_file_name nb_digits
-    printf "Input  : %d\n" nb_digits
-    printf "Result : %d\n" (v::Int)
-    printf "Time   : %.3fs\n" (t::Float)
+    (filename, nb_digits) <- read_string_int
+    file_content <- readFile filename
+    stat_func getLargestProduct (file_content, nb_digits)
 
-getLargestProduct file_name nb_digits = do
-        content <- readFile file_name
-        let arrays = concat . map (sublist nb_digits) . splitOn "0" . filter (\x -> x /= '\n') $ content in do
-            return . maximum . map listproduct $ arrays
+getLargestProduct (file_content, nb_digits) =
+        maximum . map listproduct . concat . map (sublist nb_digits) . splitOn "0" . concat . lines $ file_content
 
 listproduct list = product . map ((\x y -> read [(x!!y)] :: Int) list) $ [0 .. length list - 1]
+
 sublist len list = map (\x -> take len . drop x $ list) [0 .. length list - len]
 
