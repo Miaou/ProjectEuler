b4a9a76a4d64a170f46450c67bc083e65e68605c
Miaou <miaou.pbl@gmail.com>
2017-05-31 13:20:46 +0200

    Adapt Haskell utils

--- lib/utils.hs
+++ lib/utils.hs
@@ -1,3 +1,7 @@
+{-|
+A module to fetch most common command line arguments
+and display time informations.
+-}
 module Euler.Utils where
 
 import System.Environment (getArgs)
@@ -6,24 +10,30 @@ import System.CPUTime (getCPUTime)
 import Text.Printf (printf)
 
 
+-- |Int depends on the implementation, usually an Int64
+read_string_int :: IO (String, Int)
 read_string_int = do
     args <- getArgs
     if length args /= 2
       then error "There shall be exactly two arguments (string, int)."
       else return (args !! 0 :: String , read $ args !! 1 :: Int)
 
+-- |Int depends on the implementation, usually an Int64
+read_int :: IO Int
 read_int = do
     args <- getArgs
     if length args /= 1
       then error "There shall be exactly one argument (int)."
       else return (read $ head args :: Int)
 
+read_string_integer :: IO (String, Integer)
 read_string_integer = do
     args <- getArgs
     if length args /= 2
       then error "There shall be exactly two arguments (string, integer)."
       else return (args !! 0 :: String , read $ args !! 1 :: Integer)
 
+read_integer :: IO Integer
 read_integer = do
     args <- getArgs
     if length args /= 1
@@ -32,13 +42,16 @@ read_integer = do
 
 echo_err str = hPutStrLn stderr str
 
--- See https://wiki.haskell.org/Timing_computations for better time computations
-stat_func :: (Show a, Show b) => (a -> b) -> a -> IO c
+-- |Invoke the function and display time information. Also returns the number of millisec
+--  See https://wiki.haskell.org/Timing_computations for better time computations
+stat_func :: (Show a, Show b) => (a -> b) -> a -> IO Int
 stat_func f args = do
     start <- getCPUTime
     x <- return $! f args
     end <- getCPUTime
+    let ms = (truncate . (1e-9*) . fromIntegral) (end-start) :: Int
     printf "Input  : %s\n" (show args)
     printf "Result : %s\n" (show x)
-    printf "Time   : %d ms\n" ((truncate . (1e-9*) . fromIntegral) (end-start) :: Int)
+    printf "Time   : %d ms\n" (ms)
+    return ms
 
